1ecf0a749c8e6345d48c9f64e71f60ff4ba313c2
sevbch <sevbch@gmail.com>
2024-02-13 21:20:38 +0100

    fix conflict

--- bash_scripts/activate_timedRun.sh
+++ bash_scripts/activate_timedRun.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 # 1: IP adress with opened port 5500, or phone identifier (from 'adb devices')
-=======
-# 1: IP adress with opened port 5500
->>>>>>> e68115b14a84ab47abc8b02015a1cb59cd260f0c
 # 2: time befort starting experiments (in seconds)
 # 3: duration of experiments (in seconds)
 
@@ -18,3 +14,4 @@ adb -s $1 shell input tap 860 700
 adb -s $1 shell input keyevent --longpress $(printf 'KEYCODE_DEL %.0s' {1..250})
 adb -s $1 shell input text $3 # fill in acquisiton time
 adb -s $1 shell input tap 910 1500
+x
